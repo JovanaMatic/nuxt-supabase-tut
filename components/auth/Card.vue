db0d1f9474fa3ea134b0b6a0a21ba811a5cb8e41
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-25 18:27:32 +0100

    Create login form and add auth state

--- components/auth/Card.vue
+++ components/auth/Card.vue
@@ -1,12 +1,25 @@
+<script setup lang="ts">
+  const isLoggedIn = ref<'login' | 'signup'>('signup')
+
+  const handleLoginState = () => {
+    if (isLoggedIn.value === 'login') {
+      isLoggedIn.value = 'signup'
+    } else {
+      isLoggedIn.value = 'login'
+    }
+  }
+</script>
+
 <template>
   <div>
     <UCard class="card">
-      <h3>Login</h3>
+      <h3>{{ isLoggedIn }}</h3>
       <div class="input-container">
         <input placeholder="Email" />
-        <input placeholder="Pasword" />
+        <input placeholder="Password" />
       </div>
-      <UButton>Log in</UButton>
+      <UButton>{{ isLoggedIn === 'signup' ? 'Sign up' :'Log in' }}</UButton>
+      <p @click="handleLoginState">{{ isLoggedIn === 'login' ? 'Don\'t have an account? Create one now.' : 'Already have an account? Go ahead and log in' }}</p>
     </UCard>
   </div>
 </template>
@@ -23,6 +36,7 @@
   .card h3 {
     font-size: 1.75rem;
     width: 25rem;
+    text-transform: capitalize;
   }
 
   .input-container {
@@ -36,5 +50,12 @@
     outline: none;
     border: 0.1rem solid rgba(0, 0, 0, 0.1);
     border-radius: 0.2rem;
+  }
+
+  p {
+    color: blue;
+    padding-top: 0.5rem;
+    font-size: 0.5rem;
+    cursor: pointer;
   }
 </style>
